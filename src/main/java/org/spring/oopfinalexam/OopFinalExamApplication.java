ffa9ce0032cf8142c714becbca5e21e6fb7be1bd
fatratra-png <nyfatratrar@gmail.com>
2026-08-19 17:10:05 +0300

    Initial commit

---
+++ src/main/java/org/spring/oopfinalexam/OopFinalExamApplication.java
@@ -0,0 +1,13 @@
+package org.spring.oopfinalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopFinalExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopFinalExamApplication.class, args);
+    }
+
+}
