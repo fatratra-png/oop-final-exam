90123b05131c28219bf8fb2699ecfa6d3c15a1b6
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-08-20 15:35:04 +0300

    feat: add AccountType and TransactionType enums in model

---
+++ src/main/java/org/spring/oopfinalexam/model/AccountType.java
@@ -0,0 +1,7 @@
+package org.spring.oopfinalexam.model;
+
+public enum AccountType {
+    STANDARD,
+    PREMIUM,
+    GOLD
+}
---
+++ src/main/java/org/spring/oopfinalexam/model/TransactionType.java
@@ -0,0 +1,6 @@
+package org.spring.oopfinalexam.model;
+
+public enum TransactionType {
+    IN,
+    OUT
+}
