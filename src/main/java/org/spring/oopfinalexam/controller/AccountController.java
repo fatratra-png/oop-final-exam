591afd067f69f9979aa45e5c4e20d94fdb61fc7b
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-08-20 15:37:02 +0300

    feat: add AccountController REST endpoints

---
+++ src/main/java/org/spring/oopfinalexam/controller/AccountController.java
@@ -0,0 +1,32 @@
+package org.spring.oopfinalexam.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.spring.oopfinalexam.model.Transaction;
+import org.spring.oopfinalexam.service.AccountService;
+
+@RestController
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/accounts/{id}/transactions")
+    public List<Transaction> getTransactionsForAccount(@PathVariable UUID id) {
+        return accountService.getTransactionsForAccount(id);
+    }
+
+    @GetMapping("/account/{id}/balance")
+    public BigDecimal getBalance(@PathVariable UUID id) {
+        return accountService.getBalance(id);
+    }
+}
